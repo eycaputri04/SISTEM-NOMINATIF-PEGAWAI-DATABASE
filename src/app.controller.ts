1cb25933fe42dc1e1aeaa6cc1ba40b6eca0e367e
eycaputri04 <eycaputri04@gmail.com>
2025-11-07 19:40:46 +0700

    edit app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,8 +9,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-  @Get('api')
-  getApiInfo(): string {
-    return 'Ini adalah endpoint /api';
-  }
 }
