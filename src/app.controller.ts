8956085c131846f629340f22295c0021c0a00577
eycaputri04 <eycaputri04@gmail.com>
2025-11-07 19:56:12 +0700

    edit app.controller.ts & app.service.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('api')
+  getApiInfo(): string {
+    return 'Ini adalah endpoint /api';
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<h1>SISNOMPEG Admin API</h1><p>Lihat dokumentasi di <a href="/api-docs">/api-docs</a></p>';
+    return 'Hello World!';
   }
 }
